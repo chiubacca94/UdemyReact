f16115f972e58dea0df9e9503d2b52269ca3a5d0
Brittany Chiu <bc.chiu12@gmail.com>
2022-04-28 23:33:53 -0400

    updae

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// This is only done once for the root component
 ReactDOM.render(<App />, document.getElementById('root'));
