c61c5a73de2b7348a1fc0d23227dd8c5e8297fbe
Brittany Chiu <bc.chiu12@gmail.com>
2024-11-15 23:05:14 -0500

    added vite stuff

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom';
-
-import './index.css';
-import App from './App';
-
-// This is only done once for the root component
-ReactDOM.render(<App />, document.getElementById('root'));
