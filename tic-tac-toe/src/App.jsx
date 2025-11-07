30c89fc68d73d41278deeaf56e3b70d323aeb323
Brittany Chiu <hi@brittanychiu.com>
2025-11-07 03:02:20 -0500

    add stuff to ttt

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -4,16 +4,19 @@ function App() {
   
   return (
     <main>
-      <h1>React Tic-Tac-Toe</h1>
       <div id="game-container">
-        PLAYERS
         <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
-        </ol>
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol"> X </span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol"> O </span>
+          </li>
+        </ol> 
         GAMEBOARD
       </div>
-      LOG
     </main>
   )
 }
