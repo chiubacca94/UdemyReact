68b90255f3e92982d8cac600e4bd187aa168e58c
Brittany Chiu <hi@brittanychiu.com>
2025-11-19 21:47:38 -0500

    final

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -3,8 +3,35 @@ import { useState } from "react"
 import Player from "./components/Player"
 import GameBoard from "./components/Gameboard"
 import Log from "./components/Log"
+import GameOver from "./components/GameOver"
 
 import { WINNING_COMBINATIONS } from "./winnging-combinations"
+
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2',
+}
+
+const INITIAL_GAMEBOARD = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
+
+function derviveGameboard(turns) {
+let gameboard = [...INITIAL_GAMEBOARD.map(array => [...array])];
+
+  // Deriving state from gameturns in the App component
+  for (const turn of gameTurns) {
+        // square: {row: rowIndex, col: colIndex}
+        const { square, player } = turn;
+        const { row, col } = square;
+
+        gameboard[row][col] = player;
+
+    }
+    return gameboard;
+}
 
 function deriveActivePlayer(turns) {
   let currentPlayer = 'X';
@@ -16,10 +43,36 @@ function deriveActivePlayer(turns) {
   return currentPlayer; 
 }
 
+function derviveWinner(gameboard) {
+let winner = null;
+
+    for (const combination of WINNING_COMBINATIONS) {
+      const firstSquareSymbol = gameboard[combination[0].row][combination[0].column];
+      const secondSquareSymbol = gameboard[combination[1].row][combination[1].column];
+      const thirdSquareSymbol = gameboard[combination[2].row][combination[2].column];
+
+      if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+        console.log(`Winner is ${firstSquareSymbol}`);
+        winner = playerNames[firstSquareSymbol];
+
+
+      }
+    }
+    return winner;
+}
+
 function App() {
+  // save player names in state on save
+  const [playerNames, setPlayerNames] = useState(PLAYERS);
   const [gameTurns, setGameTurns] = useState([]);
 
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  const gameboard = derviveGameboard(gameTurns);
+
+  const winner = derviveWinner(gameboard);
+  
+  const isDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns(prevTurns => {
@@ -33,15 +86,27 @@ function App() {
       return updatedTurns;
     })
   }
+
+  function handlePlayAgain() {
+    setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayerNames(prevNames => ({
+      ...prevNames,
+      [symbol]: newName,
+    }));
+  }
   
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={ activePlayer =='X' }/>
-          <Player initialName="Player 2" symbol="O" isActive={ activePlayer =='O' }/>
+          <Player initialName={PLAYERS.X} symbol="X" isActive={ activePlayer =='X' } onChangeName={handlePlayerNameChange} />
+          <Player initialName={PLAYERS.O} symbol="O" isActive={ activePlayer =='O' } onChangeName={handlePlayerNameChange} />
         </ol> 
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
+        { (winner || isDraw) && <GameOver winner={winner} onRestart={handlePlayAgain}/>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameboard} />
       </div>
       <Log turns={gameTurns}/>
     </main>
--- tic-tac-toe/src/components/Player.jsx
+++ tic-tac-toe/src/components/Player.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
     const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
         setIsEditing((editing) => !editing);
+
+        if (isEditing) {
+            // save button clicked - update the name in parent component
+            onChangeName(symbol, playerName);
+        }
     }
 
     function handleChange(event) {
         // triggered upon some event like enter or paste character into input field
-        console.log(event);
         setPlayerName(event.target.value);
     }
     
