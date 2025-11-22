5463d9e74dfacc96043947f92c6c327288e89716
Brittany Chiu <hi@brittanychiu.com>
2025-11-21 23:26:53 -0500

    prelift of userInput

--- 05-react-essentials/src/components/UserInput.jsx
+++ 05-react-essentials/src/components/UserInput.jsx
@@ -1,7 +1,46 @@
-export default function UserInput() {   
+import { useState } from "react";
+
+export default function UserInput() {  
+    const [UserInput, setUserInput] = useState(
+        {
+            "initialInvestment": 1000,
+            "annualInvestment": 1200,
+            "expectedReturn": 6,
+            "duration": 10
+        }
+    ); 
+
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput((prevUserInput) => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: newValue
+            }
+        })
+    }
+
     return (
         <section id="user-input">
-            {/* User input form will go here */}
+            <div className="input-group">
+                <p>
+                    <label>Initial Investment</label>
+                    <input type="number" value={UserInput.initialInvestment} required onChange={(event) => handleChange("initialInvestment", event.target.value)}/>
+                </p>
+                <p>
+                    <label>Annual Investment</label>
+                    <input type="number" value={UserInput.annualInvestment} required onChange={(event) => handleChange("annualInvestment", event.target.value)}/>
+                </p>
+            </div>
+            <div className="input-group">
+                <p>
+                    <label>Expected Return</label>
+                    <input type="number" value={UserInput.expectedReturn} required onChange={(event) => handleChange("expectedReturn", event.target.value)}/>
+                </p>
+                <p>
+                    <label>Duration</label>
+                    <input type="number" value={UserInput.duration} required onChange={(event) => handleChange("duration", event.target.value)}/>
+                </p>
+            </div>
         </section>
     );
 }
