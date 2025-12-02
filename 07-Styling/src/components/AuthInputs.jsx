ca5635c288e0711659cd46db598dcdf7c832c50a
Brittany Chiu <hi@brittanychiu.com>
2025-12-01 22:24:22 -0500

    tailwind

--- 07-Styling/src/components/AuthInputs.jsx
+++ 07-Styling/src/components/AuthInputs.jsx
@@ -3,16 +3,6 @@ import { useState } from 'react';
 import Button from './Button';
 import Input from './Input';
 
-import { styled } from 'styled-components';
-
-// tagged template literal javaScript feature
-const ControlledContainer = styled.div`
- display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
-
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -34,9 +24,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      {/* used to be <div className="controls"> */}
-      <ControlledContainer>
+    <div id="auth-inputs" className='w-fill max-w-sm p-8 mx-auto rounded shadow-md bg-linear-to-b from-stone-700 to-stone-800'>
+      <div className='flex flex-col gap-2 mb-6'>
         <Input 
           label="Email"
           invalid={emailNotValid}
@@ -49,12 +38,12 @@ export default function AuthInputs() {
           type="password"
           onChange={(event) => handleInputChange('password', event.target.value)}
         />
-      </ControlledContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
-        <Button className='button' onClick={handleLogin}>Sign In</Button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
--- 07-Styling/src/components/Input.jsx
+++ 07-Styling/src/components/Input.jsx
@@ -1,56 +1,19 @@
-import { styled } from 'styled-components';
+export default function Input({label, invalid, ...props}) {
+  let labelClasses = "block mb-2 text-xs font-bold tracking-wide uppercase";   
+  let inputClasses = "w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow";
+ 
+  if (invalid){
+    labelClasses += " text-red-500";
+    inputClasses += " bg-red-200 border-red-500";
+  } else {
+    labelClasses += " text-stone-700";
+    inputClasses += " bg-stone-200 border-stone-300 focus:border-amber-400 focus:bg-white focus:outline-none";
+  }
 
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({invalid}) => invalid ? '#f87171' : '#6b7280' };
-`;
-// color: #6b7280;
-
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({invalid}) => invalid ? '#ef4444' : '#d1d5db'};
-  color: ${({invalid}) => invalid ? '#fed2d2' : '#374151'};
-  border: 1px solid ${({invalid}) => invalid ? '#f73f3f' : 'transparent' };
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
-
-export default function CustomInput({label, invalid, ...props}) {
-    return (
+  return (
     <p>
-        <Label $invalid={invalid}>{label}</Label>
-        <Input $invalid={invalid} {...props} />
+      <label className={labelClasses}>{label}</label>
+      <input className={inputClasses} {...props} />
     </p>
     );
 }
-
-// If no invalid in args, props.invalid is used
-
-// Replacing this..
-// Second way
-{/* <p>
-        <Label invalid={emailNotValid}>Email</Label>
-        <Input invalid={emailNotValid} onChange={(event) => handleInputChange('email', event.target.value)} />
-    </p> */}
-
-    // First way
-    // <p>
-    //       <Label className={`label ${passwordNotValid ? 'invalid' : ''}`}>Password</Label>
-    //       <Input
-    //         type="password"
-    //         // style={{
-    //         //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
-    //         // }}
-    //         className={passwordNotValid ? 'invalid' : undefined}
-    //         onChange={(event) =>
-    //           handleInputChange('password', event.target.value)
-    //         }
-    //       />
-    //     </p>
