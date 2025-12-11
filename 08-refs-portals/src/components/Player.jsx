968907c61d3fc1938d47d42d8a973ade45d91b4c
Brittany Chiu <hi@brittanychiu.com>
2025-12-11 17:16:29 -0500

    player

--- 08-refs-portals/src/components/Player.jsx
+++ 08-refs-portals/src/components/Player.jsx
@@ -1,10 +1,21 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+
+  const playerName = useRef();
+  const [enteredPlayerName, setPlayerName] = useState(null);
+
+  function handleNameSubmit() {
+    setPlayerName(playerName.current.value);
+    playerName.current.value = ''; // imperative clearing of input field (change of DOM directly is technically against react rules) 
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text"/>
+        <button onClick={handleNameSubmit}>Set Name</button>
       </p>
     </section>
   );
